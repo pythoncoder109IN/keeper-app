b287ebf405bb686b54d7e9ec2f969bdb7573b30b
Harsh Sharma <120568101+HarshSharmaIN@users.noreply.github.com>
2024-03-29 19:52:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
