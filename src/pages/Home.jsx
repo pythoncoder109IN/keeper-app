8db2d53c70a383d9509c841da128a45a9cbfc109
Harsh Sharma <120568101+HarshSharmaIN@users.noreply.github.com>
2024-04-21 15:10:46 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,7 +49,9 @@ function Home() {
           >
             Logout
           </Button>
-        ) : (<Backdrop
+        ) : (
+          console.log("backdrop activated");
+          <Backdrop
           sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
           open={showNotes}
         >
